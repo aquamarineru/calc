81e4ae3489be6b62bec1dd349d4daef1dc2ade63
Konstantinos <zeus_kosta@hotmail.com>
2022-12-30 21:07:39 +0100

    end of script

--- src/index.js
+++ src/index.js
@@ -12,21 +12,46 @@ const equalsHTMLSelector = document.querySelector("#calculator .eval");
 const historyUL = document.querySelector('#history');
 
 class Calculatror {
-    constructor (){
+    constructor() {
         //State => stuff that chages
         this._histroy = [];
     }
     //push date in empty array
-    history () {
+    history() {
         this._histroy.push(calculatorScreen.innerHTML); 
         historyUL.innerHTML += `<li>${calculatorScreen.innerHTML} = ${this.equals()}</li>`;
     }
+    equals() {
+        return eval(calculatorScreen.innerHTML.replaceAll("x","*"));
+    }
+    clear() {
+        calculatorScreen.innerHTML = ""; 
+    }
 }
+const myCalculator = new Calculatror();
 
 
         
     // add calculation to this ._history
     // add current calculatio to #history UL
+    function writeValue (value){
+        calculatorScreen.innerHTML += value; 
+    }
+
+    document.querySelectorAll("#calculator span").forEach((key) => {
+        if (key.innerText !== "=") {
+            key.addEventListener("click", (e) => print(e.target.innerText));
+        }
+    });
+
+    equalsHTMLSelector.addEventListener('click', () => {
+        myCalculator.history()
+        calculatorScreen.innerHTML = myCalculator.equals();
+    });
+    
+    window.onload = function () {
+        calculatorScreen.focus();
+    };
 /**
  * This function below write the value of the pressed key on the screen
  * The += is the equivalent of:
